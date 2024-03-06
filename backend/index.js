a03639f8aa324bf9488df397e179f12fe0353687
unknown <you@example.com>
2024-03-06 12:23:39 +0600

    Connect with database

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,36 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// mongodb connection
+const uri =
+  "mongodb+srv://mdbaizedhasans:AeqYboGzlGXTFJch@mern.zy6vr7t.mongodb.net/?retryWrites=true&w=majority&appName=Mern";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hey Sinamika!");
@@ -10,3 +40,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// mdbaizedhasans
+// AeqYboGzlGXTFJch
