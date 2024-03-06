9f4b43b9bbd3600343902de6976e7e5b813e4cb4
unknown <you@example.com>
2024-03-06 14:22:19 +0600

    Create routes for classes(1:50)

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 // mongodb connection
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mern.zy6vr7t.mongodb.net/?retryWrites=true&w=majority&appName=Mern`;
@@ -30,6 +33,61 @@ async function run() {
     const paymentsCollection = database.collection("payments");
     const enrolledCollection = database.collection("enrolled");
     const appliedCollection = database.collection("applied");
+
+    // classes routes
+    app.post("/new-class", async (req, res) => {
+      const newClass = req.body;
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result);
+    });
+
+    app.get("/classes", async (req, res) => {
+      const query = { status: "approved" };
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get classes by instructor email address
+    app.get("/classes/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { instructorEmail: email };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // manage classes
+    app.get("/classes-manage", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // update classes status and reason
+    app.patch("/change-status/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const reason = req.body.reason;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          status: status,
+          reason: reason,
+        },
+      };
+      const result = await classesCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // get approved classes
+    app.get("/approved-classes", async (req, res) => {
+      const query = { status: "approved" };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
